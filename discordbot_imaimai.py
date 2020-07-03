e9beb24b0ba629825d6d26f812fb19edbd6e1d27
somey-sv <65824907+somey-sv@users.noreply.github.com>
2020-07-03 20:36:55 +0900

    Update discordbot_imaimai.py

--- discordbot_imaimai.py
+++ discordbot_imaimai.py
@@ -61,7 +61,7 @@ def deck_arche_analysis(sv_deck, sv_class):
         if sv_deck.count("6_B9A") == 3:
             R1 += 1
             return list(arche_dict["R"].keys())[0]
-        elif sv_deck.count("5-K1I") == 3:
+        elif sv_deck.count("6td16") == 3:
             R2 += 1
             return list(arche_dict["R"].keys())[1]
         else:
