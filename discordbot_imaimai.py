d692a3a0e65450a3dca17b8a758cfcec129a4a34
somey-sv <65824907+somey-sv@users.noreply.github.com>
2020-07-04 21:53:08 +0900

    Update discordbot_imaimai.py
    add popoimai, hawaimai

--- discordbot_imaimai.py
+++ discordbot_imaimai.py
@@ -272,6 +272,12 @@ async def on_message(message):
             analysed_data = [discord.File("class_pie_" + compe_num + ".png"),discord.File("class_bar_" + compe_num + ".png"),]
             await message.channel.send(compe_info)
             await message.channel.send(files=analysed_data)
+            
+    elif "ハワイマイ" in message.content:
+        await message.channel.send(file=discord.File("hawaimai.jpg"))
+        
+    elif "ポポイマイ" in message.content:
+        await message.channel.send(file=discord.File("popoimai.jpeg"))
 
     elif 'イマイ' in message.content:
         await message.channel.send('Hi.')
