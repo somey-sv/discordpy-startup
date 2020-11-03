73bd596e1e8a5dde6c503c6a272415afa2091cca
somey-sv <65824907+somey-sv@users.noreply.github.com>
2020-11-03 21:47:22 +0900

    Update discordbot_imaimai.py

--- discordbot_imaimai.py
+++ discordbot_imaimai.py
@@ -204,7 +204,7 @@ async def on_message(message):
         fig3 = plt.figure()
         x = np.array(list(range(len(class_label))))
         plt.bar(x, pick_results, color=class_colors)
-        plt.ylabel("使用数",font_properties=fontprop)
+        plt.ylabel("勝利数",font_properties=fontprop)
         plt.xticks(x,class_label,rotation=90,font_properties=fontprop)
         plt.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.95)
         for x, y in zip(x, pick_results):
