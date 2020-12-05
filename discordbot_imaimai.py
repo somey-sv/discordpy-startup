550e4f3e05852af6fb0f9215ef321e5403f6991b
somey-sv <65824907+somey-sv@users.noreply.github.com>
2020-12-05 23:53:56 +0900

    Update discordbot_imaimai.py

--- discordbot_imaimai.py
+++ discordbot_imaimai.py
@@ -497,7 +497,7 @@ async def on_message(message):
     elif "ミスティング・ポポ" in message.content:
         await message.channel.send(file=discord.File("mysting_popo.png"))
 
-    elif message.isMemberMentioned(client.user):
+    elif client.user in message.mentions:
         generator = GenerateText(random.randint(1,3))
         markovstring = generator.generate()
         await message.channel.send(markovstring)
