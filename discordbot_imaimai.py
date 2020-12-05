3c7d5052bf49f53289c76c07ccf87ec332b99775
somey-sv <65824907+somey-sv@users.noreply.github.com>
2020-12-05 23:42:08 +0900

    Update discordbot_imaimai.py
    markov

--- discordbot_imaimai.py
+++ discordbot_imaimai.py
@@ -14,6 +14,150 @@ from matplotlib.font_manager import FontProperties
 
 fontprop = FontProperties(fname="ipaexg.ttf")
 
+import random
+import sqlite3
+
+class GenerateText(object):
+    """
+    文章生成用クラス
+    """
+
+    def __init__(self, n=1):
+        """
+        初期化メソッド
+        @param n いくつの文章を生成するか
+        """
+        self.n = n
+
+    def generate(self):
+        """
+        実際に生成する
+        @return 生成された文章
+        """
+
+        # DBオープン
+        con = sqlite3.connect("chain.db")
+        con.row_factory = sqlite3.Row
+
+        # 最終的にできる文章
+        generated_text = ""
+
+        # 指定の数だけ作成する
+        for i in range(self.n):
+            text = self._generate_sentence(con)
+            generated_text += text
+
+        # DBクローズ
+        con.close()
+
+        return generated_text
+
+    def _generate_sentence(self, con):
+        """
+        ランダムに一文を生成する
+        @param con DBコネクション
+        @return 生成された1つの文章
+        """
+        # 生成文章のリスト
+        morphemes = []
+        # はじまりを取得
+        first_triplet = self._get_first_triplet(con)
+        morphemes.append(first_triplet[1])
+        morphemes.append(first_triplet[2])
+
+        # 文章を紡いでいく
+        while morphemes[-1] != "__END_SENTENCE__":
+            prefix1 = morphemes[-2]
+            prefix2 = morphemes[-1]
+            triplet = self._get_triplet(con, prefix1, prefix2)
+            morphemes.append(triplet[2])
+
+        # 連結
+        result = "".join(morphemes[:-1])
+
+        return result
+
+    def _get_chain_from_DB(self, con, prefixes):
+        """
+        チェーンの情報をDBから取得する
+        @param con DBコネクション
+        @param prefixes チェーンを取得するprefixの条件 tupleかlist
+        @return チェーンの情報の配列
+        """
+        # ベースとなるSQL
+        sql = "select prefix1, prefix2, suffix, freq from chain_freqs where prefix1 = ?"
+
+        # prefixが2つなら条件に加える
+        if len(prefixes) == 2:
+            sql += " and prefix2 = ?"
+
+        # 結果
+        result = []
+
+        # DBから取得
+        cursor = con.execute(sql, prefixes)
+        for row in cursor:
+            result.append(dict(row))
+
+        return result
+
+    def _get_first_triplet(self, con):
+        """
+        文章のはじまりの3つ組をランダムに取得する
+        @param con DBコネクション
+        @return 文章のはじまりの3つ組のタプル
+        """
+        # BEGINをprefix1としてチェーンを取得
+        prefixes = ("__BEGIN_SENTENCE__",)
+
+        # チェーン情報を取得
+        chains = self._get_chain_from_DB(con, prefixes)
+
+        # 取得したチェーンから、確率的に1つ選ぶ
+        triplet = self._get_probable_triplet(chains)
+
+        return (triplet["prefix1"], triplet["prefix2"], triplet["suffix"])
+
+    def _get_triplet(self, con, prefix1, prefix2):
+        """
+        prefix1とprefix2からsuffixをランダムに取得する
+        @param con DBコネクション
+        @param prefix1 1つ目のprefix
+        @param prefix2 2つ目のprefix
+        @return 3つ組のタプル
+        """
+        # BEGINをprefix1としてチェーンを取得
+        prefixes = (prefix1, prefix2)
+
+        # チェーン情報を取得
+        chains = self._get_chain_from_DB(con, prefixes)
+
+        # 取得したチェーンから、確率的に1つ選ぶ
+        triplet = self._get_probable_triplet(chains)
+
+        return (triplet["prefix1"], triplet["prefix2"], triplet["suffix"])
+
+    def _get_probable_triplet(self, chains):
+        """
+        チェーンの配列の中から確率的に1つを返す
+        @param chains チェーンの配列
+        @return 確率的に選んだ3つ組
+        """
+        # 確率配列
+        probability = []
+
+        # 確率に合うように、インデックスを入れる
+        for (index, chain) in enumerate(chains):
+            for j in range(chain["freq"]):
+                probability.append(index)
+
+        # ランダムに1つを選ぶ
+        chain_index = random.choice(probability)
+
+        return chains[chain_index]
+
+
+
 #どばすぽのヘッダー部分
 dbsp_header = "https://shadowverse-portal.com/deck/"
 
@@ -354,7 +498,9 @@ async def on_message(message):
         await message.channel.send(file=discord.File("mysting_popo.png"))
 
     elif 'イマイ' in message.content:
-        await message.channel.send('Hi.')
+        generator = GenerateText(random.randint(1,3))
+        markovstring = generator.generate()
+        await message.channel.send(markovstring)
 
     elif message.content == "リスト":
         await message.channel.send(arche_list)
